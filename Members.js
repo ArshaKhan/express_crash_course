fe06df5d70e2287c7d1461defcb618afa61f4532
ArshaKhan <66750544+ArshaKhan@users.noreply.github.com>
2020-06-11 00:08:15 +0400

    Add files via upload

---
+++ Members.js
@@ -0,0 +1,23 @@
+
+const members = [
+    {
+        id: 1,
+        name: 'Jon Snow',
+        email: 'jonsnow@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Danarys Tar',
+        email: 'queenofdragons@gamil.com',
+        status:'inactive'
+    },
+    {
+        id: 3,
+        name: 'Arya Stark',
+        email: 'noone@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
